b682c79583baa7300e08c673577fc10900046661
alimjeremy <jeremyalim11@gmail.com>
2025-12-04 17:18:29 +0800

    M2 ACTIVITY2 DONE

---
+++ Seatwork2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bpi.Seatwork2;
+
+/**
+ * 
+ */
+public class Seatwork2 {
+
+	public static void main(String[] args) {
+		
+		WhiteBoard wb = new WhiteBoard();
+		
+		int result = wb.addNumbers(5,3);
+		System.out.println("The total is: " +String.valueOf(result));
+
+	}
+
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,9 @@
+package com.bpi.Seatwork2;
+
+public class WhiteBoard {
+	int addNumbers(int addend1, int addend2) {
+		int value = addend1 + addend2;
+		return value;
+	}
+
+}
